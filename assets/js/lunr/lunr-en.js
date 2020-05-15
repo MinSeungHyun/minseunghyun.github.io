37bab9accab4437b8c0846b05644725c3e5e9bc8
SeungHyunMin <contact@seunghyun.in>
2020-05-15 12:07:52 +0900

    검색 기능 추가

--- assets/js/lunr/lunr-en.js
+++ assets/js/lunr/lunr-en.js
@@ -42,31 +42,15 @@ $(document).ready(function() {
     resultdiv.prepend('<p class="results__found">'+result.length+' {{ site.data.ui-text[site.locale].results_found | default: "Result(s) found" }}</p>');
     for (var item in result) {
       var ref = result[item].ref;
-      if(store[ref].teaser){
-        var searchitem =
-          '<div class="list__item">'+
-            '<article class="archive__item" itemscope itemtype="https://schema.org/CreativeWork">'+
-              '<h2 class="archive__item-title" itemprop="headline">'+
-                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
-              '</h2>'+
-              '<div class="archive__item-teaser">'+
-                '<img src="'+store[ref].teaser+'" alt="">'+
-              '</div>'+
-              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
-            '</article>'+
-          '</div>';
-      }
-      else{
-    	  var searchitem =
-          '<div class="list__item">'+
-            '<article class="archive__item" itemscope itemtype="https://schema.org/CreativeWork">'+
-              '<h2 class="archive__item-title" itemprop="headline">'+
-                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
-              '</h2>'+
-              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
-            '</article>'+
-          '</div>';
-      }
+      var searchitem =
+        '<div class="list__item">'+
+          '<article class="archive__item" itemscope itemtype="https://schema.org/CreativeWork">'+
+            '<h2 class="archive__item-title" itemprop="headline">'+
+              '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
+            '</h2>'+
+            '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,100).join(" ")+'...</p>'+
+          '</article>'+
+        '</div>';
       resultdiv.append(searchitem);
     }
   });
